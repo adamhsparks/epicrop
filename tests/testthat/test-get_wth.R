9ef9bf45446db0a2a43653f600e59750a7bf5db7
Adam Sparks <adamhsparks@gmail.com>
2020-11-29 21:13:35 +1000

    Add a few tests so that the package at least has some...

---
+++ tests/testthat/test-get_wth.R
@@ -0,0 +1,11 @@
+
+# weather handling and checking ------------------------------------------------
+context("Weather data is properly retrieved")
+test_that("Weather data is as expected after fetching from POWER API", {
+  wth <- get_wth(lonlat = c(151.81, -27.48),
+                 dates = c("2015-01-15", "2015-05-15"))
+  expect_named(wth,
+               c("YYYYMMDD", "DOY", "TM", "TN", "TX",
+                 "RH", "RAIN", "LON", "LAT"))
+  expect_is(wth, c("data.table", "data.frame"))
+})
