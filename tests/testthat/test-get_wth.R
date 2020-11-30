b6156b279e526c3ad793c9668982f93604df2ca6
Adam Sparks <adamhsparks@gmail.com>
2020-11-30 12:27:38 +1000

    Format code in test

--- tests/testthat/test-get_wth.R
+++ tests/testthat/test-get_wth.R
@@ -5,7 +5,17 @@ test_that("Weather data is as expected after fetching from POWER API", {
   wth <- get_wth(lonlat = c(151.81, -27.48),
                  dates = c("2015-01-15", "2015-05-15"))
   expect_named(wth,
-               c("YYYYMMDD", "DOY", "TM", "TN", "TX",
-                 "TDEW", "RH", "RAIN", "LON", "LAT"))
+               c(
+                 "YYYYMMDD",
+                 "DOY",
+                 "TM",
+                 "TN",
+                 "TX",
+                 "TDEW",
+                 "RH",
+                 "RAIN",
+                 "LON",
+                 "LAT"
+               ))
   expect_is(wth, c("data.table", "data.frame"))
 })
