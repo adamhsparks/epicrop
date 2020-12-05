91221751ecfe77556ca28bc2bc84e3564fcb39a4
Adam Sparks <adamhsparks@gmail.com>
2020-12-05 15:38:44 +1000

    correct internal test

--- tests/testthat/test-internal_functions.R
+++ tests/testthat/test-internal_functions.R
@@ -2,5 +2,5 @@
 # saturated vapour pressure ----------------------------------------------------
 context("es calculations")
 test_that("es is properly calculated", {
- expect_equal(.saturated_vapor_pressure(0), 0.611, tolerance = 0.001)
+ expect_equal(.saturated_vapour_pressure(0), 0.6109, tolerance = 0.0001)
 })
