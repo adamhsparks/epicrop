281c593a59bfcb212be433caccca698716d8e30c
Adam Sparks <adamhsparks@gmail.com>
2020-12-08 15:23:24 +1000

    Fix missing "{" in if statement

--- R/SEIR.R
+++ R/SEIR.R
@@ -159,7 +159,7 @@ SEIR <-
 
     # check that the dates roughly align
     if (!(emergence >= wth[1, YYYYMMDD]) |
-        (max(dates) > max(wth[, YYYYMMDD])))
+        (max(dates) > max(wth[, YYYYMMDD]))) {
       stop("Incomplete weather data or dates do not align")
     }
 
