2548b57eb078d097a5def659b92a587f026828d8
Adam Sparks <adamhsparks@gmail.com>
2021-02-20 12:58:02 +0800

    Test that SEIR() switches leaf wetness types properly

--- tests/testthat/test-SEIR.R
+++ tests/testthat/test-SEIR.R
@@ -4,3 +4,8 @@ test_that("SEIR() stops if dates do not align", {
   expect_error(predict_bacterial_blight(wth, emergence = "1999-07-01"),
                regexp = "Incomplete weather data or dates do not align")
 })
+
+test_that("SEIR() switches leaf wetness types", {
+  wth <- epicrop:::wth
+  predict_bacterial_blight(wth, emergence = "2000-07-01", wetness_type = 1)
+})
