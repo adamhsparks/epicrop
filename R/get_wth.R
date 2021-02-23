938acad12cb4bdd6d8f011d4ec5461cf5a2f05b0
Adam H. Sparks <adamhsparks@gmail.com>
2021-02-23 20:29:16 +0800

    Add optional season length
    Simplify getting weather data for a given season for a single run. Just specify start date and length of season to simulate, `get_wth()` does the rest

--- R/get_wth.R
+++ R/get_wth.R
@@ -4,6 +4,9 @@
 #' @param lonlat A numeric vector of geographic coordinates for a cell or region
 #'  entered as x, y coordinates.
 #' @param dates A character vector of start and end dates in that order.
+#' @param season_length A numeric value indicating how many days a single
+#'  growing season being simulated is to be. If this is used, only the start
+#'  date will be used and the end date will be ignored if supplied.
 #'
 #' @return A \code{\link[data.table]{data.table}} of weather data, dates and
 #'  geolocation information (LAT/LON values) suitable for use in \pkg{epicrop}
@@ -31,11 +34,25 @@
 #'   lonlat = c(121.25562, 14.6774),
 #'   dates = c("2000-06-30", "2000-12-31")
 #' ))
+#' #' # get 120 days of weather for IRRI Zeigler Experiment Station in wet
+#'  season 2000
+#' (wth <- get_wth(
+#'   lonlat = c(121.25562, 14.6774),
+#'   dates = "2000-06-30",
+#'   season_length = 120
+#' ))
 #' }
 #' @author Adam H. Sparks
 #' @export get_wth
 
-get_wth <- function(lonlat, dates) {
+get_wth <- function(lonlat, dates, season_length) {
+
+  if (!missing(season_length) & length(dates != 2)) {
+    dates[2] <-
+      as.character(as.Date(as.Date(dates[1]) +
+                             (season_length + 1)))
+  }
+
   wth <- setDT(
     nasapower::get_power(
       lonlat = lonlat,
@@ -93,5 +110,4 @@ get_wth <- function(lonlat, dates) {
                 "LON"
               ))
 
-  return(wth)
 }
