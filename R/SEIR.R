ecc9ae62b5ddf5f2161cbbab23072f320e3d9b13
Adam H. Sparks <adamhsparks@gmail.com>
2021-02-25 16:57:16 +0800

    Remove internal_functions.R and move afgen() to SEIR.R

--- R/SEIR.R
+++ R/SEIR.R
@@ -302,3 +302,36 @@ SEIR <-
 
     return(res[])
   }
+
+# Original author of afgen() function is Robert J. Hijmans
+# Adapted from CRAN package meteor for epicrop package package by Adam H. Sparks
+# License GPL3
+#' @noRd
+afgen <- function(xy, x) {
+  d <- dim(xy)
+  if (x <= xy[1, 1]) {
+    res <- xy[1, 2]
+  } else if (x >= xy[d[1], 1]) {
+    res <- xy[d[1], 2]
+  } else {
+    a <- xy[xy[, 1] <= x, ]
+    b <- xy[xy[, 1] >= x, ]
+    if (length(a) == 2) {
+      int <- rbind(a, b[1, ])
+    } else if (length(b) == 2) {
+      int <- rbind(a[dim(a)[1], ], b)
+    } else {
+      int <- rbind(a[dim(a)[1], ], b[1, ])
+    }
+    if (x == int[1, 1]) {
+      res <- int[1, 2]
+    } else if (x == int[2, 1]) {
+      res <- int[2, 2]
+    } else {
+      res <- int[1, 2] + (x - int[1, 1]) *
+        ((int[2, 2] - int[1, 2]) /
+           (int[2, 1] - int[1, 1]))
+    }
+  }
+  return(res[[1]])
+}
