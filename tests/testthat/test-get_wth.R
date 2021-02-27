4af5d0bd75c9b4b2fe343d3af5a26c26a7a9dad9
Adam H. Sparks <adamhsparks@gmail.com>
2021-02-27 15:23:08 +0800

    Fix test

--- tests/testthat/test-get_wth.R
+++ tests/testthat/test-get_wth.R
@@ -19,7 +19,7 @@ test_that("Weather data is as expected after fetching from POWER API", {
 test_that("Supplying the season overrides an end-date value", {
   wth_season <- get_wth(lonlat = c(151.81, -27.48),
                         dates = c("2015-01-15", "2015-05-15"),
-                        season_length = 90)
+                        duration = 90)
   expect_equal(nrow(wth_season), 91)
 })
 
