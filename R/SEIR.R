70d911eb049ec33b80baaaea6dcd817843453542
Adam Sparks <adamhsparks@icloud.com>
2021-04-11 12:43:26 +0800

    Replaces "Derived from" with "Described in"

--- R/SEIR.R
+++ R/SEIR.R
@@ -15,36 +15,34 @@
 #'   **RAIN**:\tab Mean daily rainfall (mm).\cr
 #'   }
 #' @param emergence expected date of plant emergence entered in `YYYY-MM-DD`
-#'  format. Derived from Table 1 Savary *et al.* 2012.
+#'  format. Described in Table 1 Savary *et al.* 2012.
 #' @param onset expected number of days until the onset of disease after
-#'  emergence date. Derived from Table 1 Savary *et al.* 2012.
-#' @param duration simulation duration (growing season length). Derived from
-#'  Table 1 Savary *et al.* 2012.
+#'  emergence date. Described in Table 1 Savary *et al.* 2012.
+#' @param duration simulation duration *i.e.*, growing season length (day).
+#'  Described in Table 1 Savary *et al.* 2012.
 #' @param rhlim relative humidity value threshold to decide whether leaves are
-#'  wet or not. Savary *et al.* 2012 used 90%. From Table 1 Savary
-#'  *et al.* 2012.
+#'  wet or not, defaults to 90 (numeric). Savary *et al.* 2012 used 90%.
 #' @param rainlim rainfall amount (mm) threshold to decide whether leaves are
-#'  wet or not. Savary *et al.* 2012 used 5mm. Derived from Table 1 Savary
-#'  *et al.* 2012.
-#' @param H0 initial number of plant's healthy sites. Derived from Table 1
+#'  wet or not, defaults to 5 (numeric). Savary *et al.* 2012 used 5mm.
+#' @param H0 initial number of plant's healthy sites. Described in Table 1
 #'  Savary *et al.* 2012.
-#' @param I0 initial number of infective sites. Derived from Table 1 Savary
+#' @param I0 initial number of infective sites. Described in Table 1 Savary
 #'  *et al.* 2012.
 #' @param RcA modifier for *Rc* (the basic infection rate corrected for
-#'  removals) for crop age. Derived from Table 1 Savary *et al.* 2012.
+#'  removals) for crop age. Described in Table 1 Savary *et al.* 2012.
 #' @param RcT modifier for *Rc* (the basic infection rate corrected for
-#'  removals) for temperature. Derived from Table 1 Savary *et al.* 2012.
+#'  removals) for temperature. Described in Table 1 Savary *et al.* 2012.
 #' @param RcOpt potential basic infection rate corrected for removals. Derived
 #'  from Table 1 Savary *et al.* 2012.
-#' @param i duration of infectious period (day). Derived from Table 1 Savary
+#' @param i duration of infectious period (day). Described in Table 1 Savary
 #'  *et al.* 2012.
-#' @param p duration of latent period (day). Derived from  Table 1 Savary
+#' @param p duration of latent period (day). Described in  Table 1 Savary
 #'  *et al.* 2012.
-#' @param Sx maximum number of sites. Derived from Table 1 Savary *et al.* 2012.
-#' @param a aggregation coefficient. Derived from Table 1 Savary *et al.* 2012.
+#' @param Sx maximum number of sites. Described in Table 1 Savary *et al.* 2012.
+#' @param a aggregation coefficient. Described in Table 1 Savary *et al.* 2012.
 #' @param RRS relative rate of physiological senescence. From Table 1 Savary
 #'  *et al.* 2012.
-#' @param RRG relative rate of growth. Derived from Table 1 Savary *et al.*
+#' @param RRG relative rate of growth. Described in Table 1 Savary *et al.*
 #'  2012.
 #'
 #' @references Savary, S., Nelson, A., Willocquet, L., Pangga, I., and Aunario,
