d80a81e1691d1576918a02223be61fa5ab757287
Adam Sparks <adamhsparks@icloud.com>
2021-04-11 14:21:31 +0800

    Add tests for SEIR when `a` < 1

--- tests/testthat/test-SEIR.R
+++ tests/testthat/test-SEIR.R
@@ -4,3 +4,24 @@ test_that("SEIR() stops if dates do not align", {
   expect_error(predict_bacterial_blight(wth, emergence = "1999-07-01"),
                regexp = "Incomplete weather data or dates do not align")
 })
+
+test_that("SEIR() stops if `a` < 1", {
+  wth <- epicrop:::wth
+  expect_error(
+    SEIR(
+       wth = wth,
+       emergence = emergence,
+       onset = 20,
+       duration = 120,
+       RcA = RcA,
+       RcT = RcT,
+       RcOpt = 0.61,
+       p =  6,
+       i = 19,
+       H0 = 600,
+       a = 0.5,
+       Sx = 100000,
+       RRS = 0.01,
+       RRG = 0.1
+     ))
+})
