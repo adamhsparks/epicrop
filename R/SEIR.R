959caa9a6ca8ef659df6a694dc864b7e6e35e892
Adam Sparks <adamhsparks@gmail.com>
2021-06-29 13:29:10 +0800

    Convert `wth` to a data.table object if it is not already one

--- R/SEIR.R
+++ R/SEIR.R
@@ -159,6 +159,12 @@ SEIR <-
     # CRAN NOTE avoidance
     infday <- YYYYMMDD <- lat <- lon <- LAT <- LON <- NULL #nocov
 
+    # set wth input as a data.table object if it's not already one
+    if (!inherits(wth, "data.table")) {
+      wth <- data.table::as.data.table(wth)
+    }
+
+    # check aggregation values
     if (a < 1) {
       stop(call. = FALSE,
            "`a` cannot be set to less than 1. Valid aggregation values, `a`,",
