9fbc5df49bfe3df1bb807b3cd64521670c8b93bc
Adam Sparks <adamhsparks@icloud.com>
2021-07-05 20:46:05 +0800

    Format code in tests

--- tests/testthat/test-get_wth.R
+++ tests/testthat/test-get_wth.R
@@ -5,23 +5,23 @@ test_that("Weather data is as expected after fetching from POWER API", {
   wth <- get_wth(lonlat = c(151.81, -27.48),
                  dates = c("2015-01-15", "2015-05-15"))
   expect_named(wth,
-               c(
-                 "YYYYMMDD",
+               c("YYYYMMDD",
                  "DOY",
                  "TEMP",
                  "RHUM",
                  "RAIN",
                  "LAT",
-                 "LON"
-               ))
+                 "LON"))
   expect_is(wth, c("data.table", "data.frame"))
 })
 
 test_that("Supplying the season overrides an end-date value", {
   skip_on_cran()
-  wth_season <- get_wth(lonlat = c(151.81, -27.48),
-                        dates = c("2015-01-15", "2015-05-15"),
-                        duration = 90)
+  wth_season <- get_wth(
+    lonlat = c(151.81, -27.48),
+    dates = c("2015-01-15", "2015-05-15"),
+    duration = 90
+  )
   expect_equal(nrow(wth_season), 91)
 })
 
