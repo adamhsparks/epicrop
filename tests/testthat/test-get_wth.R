90e13f5cf75bcb97b418ee92632b3dce1a614727
Adam Sparks <adamhsparks@gmail.com>
2021-09-13 16:51:00 +0800

    Use vcr with tests

--- tests/testthat/test-get_wth.R
+++ tests/testthat/test-get_wth.R
@@ -1,28 +1,36 @@
 
 # weather handling and checking ------------------------------------------------
-test_that("Weather data is as expected after fetching from POWER API", {
-  skip_on_cran()
-  wth <- get_wth(lonlat = c(151.81, -27.48),
-                 dates = c("2015-01-15", "2015-05-15"))
-  expect_named(wth,
-               c("YYYYMMDD",
-                 "DOY",
-                 "TEMP",
-                 "RHUM",
-                 "RAIN",
-                 "LAT",
-                 "LON"))
-  expect_is(wth, c("data.table", "data.frame"))
+vcr::use_cassette("fetch_power_weather", {
+  test_that("Weather data is as expected after fetching from POWER API", {
+    skip_on_cran()
+    wth <- get_wth(
+      lonlat = c(151.81, -27.48),
+      dates = c("2015-01-15", "2015-05-15"),
+      source = "nasapower"
+    )
+    expect_named(wth,
+                 c("YYYYMMDD",
+                   "DOY",
+                   "TEMP",
+                   "RHUM",
+                   "RAIN",
+                   "LAT",
+                   "LON"))
+    expect_is(wth, c("data.table", "data.frame"))
+  })
 })
 
-test_that("Supplying the season overrides an end-date value", {
-  skip_on_cran()
-  wth_season <- get_wth(
-    lonlat = c(151.81, -27.48),
-    dates = c("2015-01-15", "2015-05-15"),
-    duration = 90
-  )
-  expect_equal(nrow(wth_season), 91)
+vcr::use_cassette("season_override", {
+  test_that("Supplying the season overrides an end-date value", {
+    skip_on_cran()
+    wth_season <- get_wth(
+      lonlat = c(151.81, -27.48),
+      dates = c("2015-01-15", "2015-05-15"),
+      duration = 90,
+      source = "nasapower"
+    )
+    expect_equal(nrow(wth_season), 91)
+  })
 })
 
 test_that("Any NA values in the POWER data will emit a message", {
