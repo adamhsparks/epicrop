7bd28208dfdd25bb5d46048c2e1da06b25e569bb
Adam Sparks <adamhsparks@gmail.com>
2021-09-14 12:08:53 +0800

    Update bb tests for bug-fixed SEIR() and Rc curves

--- tests/testthat/test-predict_bacterial_blight.R
+++ tests/testthat/test-predict_bacterial_blight.R
@@ -28,37 +28,37 @@ test_that("Values are as expected", {
   expect_equal(nrow(bb), 120)
 
   expect_equal(bb[[1, "sites"]], 100)
-  expect_equal(bb[[120, "sites"]], 1120, tolerance = 0.1)
+  expect_equal(bb[[120, "sites"]], 1074.7, tolerance = 0.1)
 
   expect_equal(bb[[1, "latent"]], 0, tolerance = 0.1)
-  expect_equal(bb[[120, "latent"]], 59.5, tolerance = 0.1)
+  expect_equal(bb[[120, "latent"]], 36.1, tolerance = 0.1)
 
   expect_equal(bb[[1, "infectious"]], 0, tolerance = 0.1)
-  expect_equal(bb[[120, "infectious"]], 786.7, tolerance = 0.1)
+  expect_equal(bb[[120, "infectious"]], 762.6, tolerance = 0.1)
 
   expect_equal(bb[[1, "removed"]], 0, tolerance = 0.1)
-  expect_equal(bb[[120, "removed"]], 536.2, tolerance = 0.1)
+  expect_equal(bb[[120, "removed"]], 593.5, tolerance = 0.1)
 
   expect_equal(bb[[1, "senesced"]], 0, tolerance = 0.1)
-  expect_equal(bb[[120, "senesced"]], 2434.3, tolerance = 0.1)
+  expect_equal(bb[[120, "senesced"]], 2427.0, tolerance = 0.1)
 
   expect_equal(bb[[1, "rateinf"]], 0, tolerance = 0.1)
-  expect_equal(bb[[120, "rateinf"]], 9.7, tolerance = 0.1)
+  expect_equal(bb[[120, "rateinf"]], 8.8, tolerance = 0.1)
 
   expect_equal(bb[[1, "rtransfer"]], 0, tolerance = 0.1)
   expect_equal(bb[[120, "rtransfer"]], 0, tolerance = 0.1)
 
   expect_equal(bb[[1, "rgrowth"]], 9.6, tolerance = 0.1)
-  expect_equal(bb[[120, "rgrowth"]], 24.4, tolerance = 0.1)
+  expect_equal(bb[[120, "rgrowth"]], 24.6, tolerance = 0.1)
 
   expect_equal(bb[[1, "rsenesced"]], 1, tolerance = 0.1)
-  expect_equal(bb[[120, "rsenesced"]], 57.9, tolerance = 0.1)
+  expect_equal(bb[[120, "rsenesced"]], 59.3, tolerance = 0.1)
 
   expect_equal(bb[[1, "diseased"]], 0, tolerance = 0.1)
-  expect_equal(bb[[120, "diseased"]], 1382.5, tolerance = 0.1)
+  expect_equal(bb[[120, "diseased"]], 1392.4, tolerance = 0.1)
 
   expect_equal(bb[[1, "intensity"]], 0, tolerance = 0.1)
-  expect_equal(bb[[120, "intensity"]], 0.4, tolerance = 0.1)
+  expect_equal(bb[[120, "intensity"]], 0.42, tolerance = 0.01)
 
   expect_equal(bb[[1, "lat"]], 14.67741, tolerance = 0.00001)
   expect_equal(bb[[1, "lon"]], 121.2556, tolerance = 0.0001)
