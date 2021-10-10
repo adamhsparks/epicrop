13bf97f387e73f0136afb6011a1e1b3135c1cc76
Adam Sparks <adamhsparks@icloud.com>
2021-10-10 15:48:51 +0800

    Calculate AUDPC from the output of SEIR()

---
+++ R/calculate_audpc.R
@@ -0,0 +1,47 @@
+#' Calculate the area under the disease progress curve (AUDPC)
+#'
+#' @param x A [data.table] object that is the output of [SEIR()] or any of the
+#' `predict` family of functions.
+#'
+#' @return  A `numeric` value as `double`.
+#'
+#' @examplesIf interactive()
+#' # get weather for IRRI Zeigler Experiment Station in wet season 2000
+#' wth <- get_wth(
+#'   lonlat = c(121.25562, 14.6774),
+#'   dates = c("2000-06-30", "2000-12-31")
+#' )
+#' bb <- predict_bacterial_blight(wth, emergence = "2000-07-01")
+#' calculate_audpc(x = bb)
+#'
+#' @author Adam H. Sparks, \email{adamhsparks@@gmail.com}
+#' @references
+#' Sparks, A.H., P.D. Esker, M. Bates, W. Dall' Acqua, Z. Guo, V. Segovia, S.D.
+#' Silwal, S. Tolos, and K.A. Garrett, 2008. Ecology and Epidemiology in R:
+#' Disease Progress over Time. *The Plant Health Instructor*.
+#' DOI:[10.1094/PHI-A-2008-0129-02]https://doi.org/10.1094/PHI-A-2008-0129-02).
+#'
+#' Madden, L. V., G. Hughes, and F. van den Bosch. 2007. The Study of Plant
+#' Disease Epidemics. American Phytopathological Society, St. Paul, MN.
+#' DOI:[10.1094/9780890545058](https://doi.org/10.1094/9780890545058).
+#'
+#' @keywords internal
+#' @noRd
+
+.calculate_audpc <- function(x) {
+  n <- sum(nrow(x), -1)
+
+  meanvec <- intvec <- vector(mode = "double", length = n)
+  intensity <- x$intensity
+  simday <- x$simday
+
+  for (i in 1:n) {
+    j <- sum(i, 1)
+    meanvec[i] <- mean(c(intensity[i], intensity[j]))
+    intvec[i] <- sum(simday[j], -simday[i])
+  }
+
+  infprod <- meanvec * intvec
+
+  return(sum(infprod))
+}
